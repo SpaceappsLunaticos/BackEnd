c50a6e0a53a6dca121fb01db8f6a64dbdcc79237
maasencioh <maasencioh@gmail.com>
2016-04-23 15:32:35 -0500

    Users basic creation

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,7 +9,6 @@ var mount = require('koa-mount');
 module.exports = function(app) {
 
 	// YEOMAN INJECT ROUTES BELOW
-  app.use(mount('/', require('../resources/root')));
-
-
+	app.use(mount('/users', require('../resources/users')));
+    app.use(mount('/', require('../resources/root')));
 };
